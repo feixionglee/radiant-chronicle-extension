efaa175c504c55fa830e084e53c546d97c8e9f4b
lifeixiong <feixiongli@gmail.com>
2011-11-29 17:10:12 +0800

    comment  migration which raise problem

--- db/migrate/20090728185710_adjust_yaml_column_type.rb
+++ db/migrate/20090728185710_adjust_yaml_column_type.rb
@@ -1,6 +1,6 @@
 class AdjustYamlColumnType < ActiveRecord::Migration
   def self.up
-    change_column :versions, :yaml, :text, :limit => 64.kilobytes + 1
+    change_column :versions, :yaml, :text#, :limit => 64.kilobytes + 1
   end
 
   def self.down
